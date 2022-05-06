cd1844fb65d55ef71177739ee70a9ed7809431c1
FlutLab.io <>
2022-05-06 10:43:26 +0000

    Autosave

--- lib/presentation/screen/checkout/shipping/add_shipping_details.dart
+++ lib/presentation/screen/checkout/shipping/add_shipping_details.dart
@@ -13,52 +13,12 @@ class AddShippingScreen extends StatelessWidget {
     return Scaffold(
         appBar: CheckoutAppbar(),
         body: SingleChildScrollView(
-          child: Column(children: [
-            StepperTab(selectedIdex: 1),
-            SizedBox(
-              height: 48.h,
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                left: 20.w,
-                right: 2.w,
-              ),
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: TitleText(
-                      text: 'Express Checkout',
-                      Size: 16,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 24.h,
-                  ),
-                  // Row(
-                  //   crossAxisAlignment: CrossAxisAlignment.start,
-                  //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  //   children: [
-                  //     PaymentCard(svgIcon: 'assets/icons/paypal.svg'),
-                  //     PaymentCard(svgIcon: 'assets/icons/googlepay.svg')
-                  //   ],
-                  // ),
-                  SizedBox(
-                    height: 16.h,
-                  ),
-                  AuthText(text1: 'Have An Account?', text2: 'Sign In', routeUrl: '/enterdetail'),
-                  SizedBox(
-                    height: 24.h,
-                  ),
-                  ContactInfo(),
-                  SizedBox(
-                    height: 32.h,
-                  ),
-                  ShippingAddress(),
-                ],
-              ),
-            )
-          ]),
+          child: Column(
+            children: [
+              StepperTab(selectedIdex: 1),
+              ShippingAddress(),
+            ],
+          ),
         ));
   }
 }
