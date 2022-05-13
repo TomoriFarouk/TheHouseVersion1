816d5c602d0f2551cd6ee96b41940ebdf299f6ef
FlutLab.io <>
2022-05-13 13:20:28 +0000

    Autosave

--- lib/presentation/screen/Listing/widget/active_listing.dart
+++ lib/presentation/screen/Listing/widget/active_listing.dart
@@ -92,7 +92,7 @@ class _ActiveListingState extends State<ActiveListing> {
 
   Widget tab({required double height, required double width, Color? color, String? text1, String? text2, Color? textColor}) {
     return Container(
-      height: height.h,
+      height: 44.h,
       width: width.w,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12),
