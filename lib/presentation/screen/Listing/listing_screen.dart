5cc784753b3a2d15495b9023c0064b76d6692748
FlutLab.io <>
2022-05-14 18:47:20 +0000

    Autosave

--- lib/presentation/screen/Listing/listing_screen.dart
+++ lib/presentation/screen/Listing/listing_screen.dart
@@ -1,124 +1,120 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_portal/flutter_portal.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:furniture_ui/presentation/resources/resource.dart';
-import 'package:furniture_ui/presentation/screen/Listing/widget/appBar.dart';
-import 'package:furniture_ui/presentation/screen/Listing/widget/listing_tab.dart';
-import 'package:furniture_ui/presentation/screen/checkout/widget/checkout_appbar.dart';
-import 'package:furniture_ui/presentation/screen/checkout/widget/stepper_tab.dart';
-import 'package:furniture_ui/presentation/widget/widget.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_portal/flutter_portal.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:furniture_ui/presentation/resources/resource.dart';
+// import 'package:furniture_ui/presentation/screen/Listing/widget/appBar.dart';
 
-import 'widget/active_listing.dart';
-import 'widget/heading.dart';
+// import 'widget/active_listing.dart';
+// import 'widget/heading.dart';
 
-class ListingScreen extends StatefulWidget {
-  @override
-  State<ListingScreen> createState() => _ListingScreenState();
-}
+// class ListingScreen extends StatefulWidget {
+//   @override
+//   State<ListingScreen> createState() => _ListingScreenState();
+// }
 
-class _ListingScreenState extends State<ListingScreen> {
-  //bool _isExpand = true;
-  bool isMenuOpen = false;
-  @override
-  Widget build(BuildContext context) {
-    return Portal(
-      child: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () {
-          if (isMenuOpen) {
-            isMenuOpen = false;
-          } else {
-            isMenuOpen = true;
-          }
-          setState(() {});
-        },
-        child: Scaffold(
-            appBar: ListingAppBar(text: 'Sell used items'),
-            body: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    Heading(),
-                    SizedBox(height: 24.h),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Active',
-                          style: Theme.of(context).textTheme.subtitle2,
-                        ),
-                        PortalTarget(
-                          visible: isMenuOpen,
-                          anchor: const Aligned(
-                            follower: Alignment.topRight,
-                            target: Alignment.topLeft,
-                          ),
-                          portalFollower: Material(
-                            elevation: 8,
-                            child: IntrinsicWidth(
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  ListTile(title: textbutton(text1: 'Active listing', text2: Routes.lisitngScreen)),
-                                  ListTile(title: textbutton(text1: 'Draft', text2: Routes.listingDraftScreen)),
-                                  ListTile(title: textbutton(text1: 'Closed', text2: Routes.lisitngClosedScreen)),
-                                ],
-                              ),
-                            ),
-                          ),
-                          child: InkWell(
-                              onTap: () {
-                                if (isMenuOpen) {
-                                  isMenuOpen = false;
-                                } else {
-                                  isMenuOpen = true;
-                                }
-                                setState(() {
-                                  //isMenuOpen = true;
-                                });
-                              },
-                              child: SvgPicture.asset('assets/icons/OpenArrow.svg', height: 10.h, width: 16.w)),
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 16.h),
-                    ActiveListing(
-                      value: 1,
-                    ),
-                    SizedBox(height: 16.h),
-                    ActiveListing(
-                      value: 1,
-                    ),
-                    SizedBox(height: 16.h),
-                    ActiveListing(
-                      value: 1,
-                    ),
-                    SizedBox(height: 16.h),
-                    ActiveListing(
-                      value: 1,
-                    )
-                  ],
-                ),
-              ),
-            )),
-      ),
-    );
-  }
+// class _ListingScreenState extends State<ListingScreen> {
+//   //bool _isExpand = true;
+//   bool isMenuOpen = false;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Portal(
+//       child: GestureDetector(
+//         behavior: HitTestBehavior.opaque,
+//         onTap: () {
+//           if (isMenuOpen) {
+//             isMenuOpen = false;
+//           } else {
+//             isMenuOpen = true;
+//           }
+//           setState(() {});
+//         },
+//         child: Scaffold(
+//             appBar: ListingAppBar(text: 'Sell used items'),
+//             body: SingleChildScrollView(
+//               child: Padding(
+//                 padding: const EdgeInsets.all(20),
+//                 child: Column(
+//                   children: [
+//                     Heading(),
+//                     SizedBox(height: 24.h),
+//                     Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                       children: [
+//                         Text(
+//                           'Active',
+//                           style: Theme.of(context).textTheme.subtitle2,
+//                         ),
+//                         PortalTarget(
+//                           visible: isMenuOpen,
+//                           anchor: const Aligned(
+//                             follower: Alignment.topRight,
+//                             target: Alignment.topLeft,
+//                           ),
+//                           portalFollower: Material(
+//                             elevation: 8,
+//                             child: IntrinsicWidth(
+//                               child: Column(
+//                                 mainAxisSize: MainAxisSize.min,
+//                                 children: [
+//                                   ListTile(title: textbutton(text1: 'Active listing', text2: Routes.lisitngScreen)),
+//                                   ListTile(title: textbutton(text1: 'Draft', text2: Routes.listingDraftScreen)),
+//                                   ListTile(title: textbutton(text1: 'Closed', text2: Routes.lisitngClosedScreen)),
+//                                 ],
+//                               ),
+//                             ),
+//                           ),
+//                           child: InkWell(
+//                               onTap: () {
+//                                 if (isMenuOpen) {
+//                                   isMenuOpen = false;
+//                                 } else {
+//                                   isMenuOpen = true;
+//                                 }
+//                                 setState(() {
+//                                   //isMenuOpen = true;
+//                                 });
+//                               },
+//                               child: SvgPicture.asset('assets/icons/OpenArrow.svg', height: 10.h, width: 16.w)),
+//                         )
+//                       ],
+//                     ),
+//                     SizedBox(height: 16.h),
+//                     ActiveListing(
+//                       value: 1,
+//                     ),
+//                     SizedBox(height: 16.h),
+//                     ActiveListing(
+//                       value: 1,
+//                     ),
+//                     SizedBox(height: 16.h),
+//                     ActiveListing(
+//                       value: 1,
+//                     ),
+//                     SizedBox(height: 16.h),
+//                     ActiveListing(
+//                       value: 1,
+//                     )
+//                   ],
+//                 ),
+//               ),
+//             )),
+//       ),
+//     );
+//   }
 
-  Widget textbutton({String? text1, String? text2}) {
-    return TextButton(
-      style: TextButton.styleFrom(primary: Colors.black),
-      onPressed: () {
-        Navigator.pushNamed(context, text2!);
-      },
-      child: Text(
-        text1!,
-        style: Theme.of(context).textTheme.bodyText1!.copyWith(
-              fontSize: 14.sp,
-            ),
-      ),
-    );
-  }
-}
+//   Widget textbutton({String? text1, String? text2}) {
+//     return TextButton(
+//       style: TextButton.styleFrom(primary: Colors.black),
+//       onPressed: () {
+//         Navigator.pushNamed(context, text2!);
+//       },
+//       child: Text(
+//         text1!,
+//         style: Theme.of(context).textTheme.bodyText1!.copyWith(
+//               fontSize: 14.sp,
+//             ),
+//       ),
+//     );
+//   }
+// }
